20ae95d15b574629da2b0b83a98688415141946d
David Allen <davidallen02@me.com>
2020-07-29 15:23:10 -0700

    Update for eval masking

--- R/line-plot.R
+++ R/line-plot.R
@@ -9,7 +9,7 @@
 lineplot <- function(df){
 
   p <- df %>%
-    ggplot2::ggplot(ggplot2::aes("dates", "value", color = "variable")) +
+    ggplot2::ggplot(ggplot2::aes(.data$dates, .data$value, color = .data$value)) +
     ggplot2::geom_line(size = 2) +
     ggplot2::scale_color_manual(values = pam.pal())
 
