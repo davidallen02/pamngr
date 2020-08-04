a5a247a4810d0ccd9bc26f5fbaa5ceb72f554d6e
David Allen <davidallen02@me.com>
2020-08-03 22:00:48 -0700

    Group by variable

--- R/percent-change.R
+++ R/percent-change.R
@@ -8,10 +8,14 @@
 #' @importFrom rlang .data
 #'
 pchange <- function(dat, k = 1){
-  dplyr::mutate(dat,
-                value = .data$value %>%
-                  magrittr::divide_by(dplyr::lag(.data$value, n = k)) %>%
-                  magrittr::subtract(1) %>%
-                  magrittr::multiply_by(100))
+
+  dat %>%
+    dplyr::group_by(variable) %>%
+    dplyr::mutate(dat,
+                  value = .data$value %>%
+                    magrittr::divide_by(dplyr::lag(.data$value, n = k)) %>%
+                    magrittr::subtract(1) %>%
+                    magrittr::multiply_by(100)) %>%
+    dplyr::ungroup()
 
 }
