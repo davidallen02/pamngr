af658e5029100238943e90b227b3cb6f25e4f325
David Allen <davidallen02@me.com>
2020-08-03 23:43:27 -0700

    clean up code

--- R/percent-change.R
+++ R/percent-change.R
@@ -11,8 +11,7 @@ pchange <- function(dat, k = 1){
 
   dat %>%
     dplyr::group_by(variable) %>%
-    dplyr::mutate(dat,
-                  value = .data$value %>%
+    dplyr::mutate(value = .data$value %>%
                     magrittr::divide_by(dplyr::lag(.data$value, n = k)) %>%
                     magrittr::subtract(1) %>%
                     magrittr::multiply_by(100)) %>%
