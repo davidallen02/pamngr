a2a0acc87810fe589685d7d0033a836d06c3e1d2
David Allen <davidallen02@me.com>
2020-09-04 09:48:45 -0700

    Add update_data()

---
+++ R/update_data.R
@@ -0,0 +1,28 @@
+#' Update all data in a directory
+#'
+#' @param security Blomberg security ID for equity or economic index
+
+update_data <- function(security){
+
+  # search for security
+  equities <- dir("R:/David/asset-management/equities")
+  economics <- dir("R:/David/economics/data")
+
+  if(security %in% equities){
+    path <- "R:/David/asset-management/equities/"
+  } else {
+    if(security %in% economics){
+      path <- "R:/David/economics/data"
+    } else{
+      print(paste(security, "not in either equities or economics folders"))
+    }
+  }
+
+  path <- path %>% paste0(security, "/data")
+
+  securities <- dir(path) %>% stringr::str_remove_all(".RDS")
+  securities <- securities[securities != "key"]
+
+  securities %>% purrr::map(pamngr::get_data)
+
+}
