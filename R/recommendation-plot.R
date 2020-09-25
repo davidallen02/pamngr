9ab1fd32e1c49d32f70ee226ce1b065e8317bfed
David Allen <davidallen02@me.com>
2020-09-25 10:48:57 -0700

    code clean up and adding commenting

--- R/recommendation-plot.R
+++ R/recommendation-plot.R
@@ -15,70 +15,69 @@ recommendation_plot <- function(ticker) {
     col_types = "cDnnnnnl") %>%
     dplyr::filter(.data$TICKER == ticker)
 
+  # Prepare recommendation range history for plotting
+  ranges <- recommendations %>%
+    dplyr::select(-c(.data$TICKER, .data$PRICE_TARGET)) %>%
+    dplyr::arrange(.data$DATE) %>%
+    dplyr::mutate(start = .data$DATE,
+                  end   = c(.data$DATE[-1], Sys.Date())) %>%
+    dplyr::select(-.data$DATE) %>%
+    reshape2::melt(id.vars = c('start', 'end', 'SOURCE')) %>%
+    dplyr::filter(!is.na(.data$value)) %>%
+    dplyr::mutate(variable = paste(.data$SOURCE, .data$variable, sep='_'))
 
-  px_last <- pamngr::get_data(ticker, type = "Equity", flds = "px-last")
-  best_target_price <- pamngr::get_data(ticker, type = "Equity", flds = "best-target-price")
-
+  # Save recommendation history start date
   stdt <- recommendations %>%
     utils::head(1) %>%
     dplyr::select(.data$DATE) %>%
     dplyr::pull()
 
-  p <- ggplot2::ggplot() +
+  # Prepare PAM internal price target data for plotting
+  pam_pt <- recommendations %>%
+    dplyr::select(.data$DATE, .data$PRICE_TARGET) %>%
+    dplyr::bind_rows(data.frame('DATE' = Sys.Date(),
+                                'PRICE_TARGET' = recommendations %>%
+                                  dplyr::select(.data$PRICE_TARGET) %>%
+                                  utils::tail(1))) %>%
+    dplyr::arrange(dplyr::desc(.data$DATE))
 
-    ggplot2::geom_segment(data = (recommendations %>%
-                                    dplyr::select(-c(.data$TICKER,
-                                                     .data$PRICE_TARGET)) %>%
-                                    dplyr::arrange(.data$DATE) %>%
-                                    dplyr::mutate(start = .data$DATE,
-                                                  end   = c(.data$DATE[-1],
-                                                            Sys.Date())) %>%
-                                    dplyr::select(-.data$DATE) %>%
-                                    reshape2::melt(id.vars = c('start', 'end', 'SOURCE')) %>%
-                                    dplyr::filter(!is.na(.data$value)) %>%
-                                    dplyr::mutate(variable = paste(.data$SOURCE,
-                                                                   .data$variable,
-                                                                   sep='_'))),
+  # Read in ticker's price and consensus price target historical data
+  px_last <- pamngr::get_data(ticker, type = "Equity", flds = "px-last")
+  best_target_price <- pamngr::get_data(ticker, type = "Equity", flds = "best-target-price")
+
+  # Join price and  consensus price target data
+  prices <- px_last %>%
+    dplyr::left_join(best_target_price, by = "dates") %>%
+    dplyr::filter(dates >= stdt) %>%
+    tidyr::pivot_longer(cols = -dates, names_to = "variable") %>%
+    tidyr::drop_na(value) %>%
+    dplyr::mutate(dates = dates %>% as.Date())
+
+  # Plot recommendation range history
+  p <- ggplot2::ggplot() +
+    ggplot2::geom_segment(data = ranges,
                           ggplot2::aes(x     = .data$start,
                                        xend  = .data$end,
                                        y     = .data$value,
                                        yend  = .data$value,
                                        color = .data$variable),
                           size  = 2,
-                          alpha = 1/3) +
+                          alpha = 1/3)
 
-    ggplot2::geom_step(data = (recommendations %>%
-                                 dplyr::select(.data$DATE, .data$PRICE_TARGET) %>%
-                                 dplyr::bind_rows(data.frame('DATE' = Sys.Date(),
-                                                             'PRICE_TARGET' = recommendations %>%
-                                                               dplyr::select(.data$PRICE_TARGET) %>%
-                                                               utils::tail(1))) %>%
-                                 dplyr::arrange(dplyr::desc(.data$DATE))),
-                       ggplot2::aes(.data$DATE, .data$PRICE_TARGET),
-                       color = '#850237',
-                       size  = 1)
+  # Add internal price target data to plot
+  p <- p + ggplot2::geom_step(data = pam_pt,
+                              ggplot2::aes(.data$DATE, .data$PRICE_TARGET),
+                              color = '#850237',
+                              size  = 1)
 
+  # Add price and consensus price target lines to plot
   p <- p + ggplot2::geom_line(
-    data = (pamngr::get_data(ticker, type = "Equity", flds = "px-last") %>%
-              magrittr::set_colnames(c("dates","PX_LAST")) %>%
-              reshape2::melt(id.vars = "dates") %>%
-              dplyr::mutate(DATE = .data$dates %>% as.Date()) %>%
-              dplyr::filter(.data$DATE >= stdt)),
-    ggplot2::aes(x = .data$DATE, y = .data$value, color = .data$variable),
-    size = 1) +
+    data = prices,
+    ggplot2::aes(x = .data$dates, y = .data$value, color = .data$variable),
+    size = 1)
 
-    ggplot2::geom_line(
-      data = (pamngr::get_data(ticker, type = "Equity", flds = "best-target-price") %>%
-                magrittr::set_colnames(c("dates", "BEST_TARGET_PRICE")) %>%
-                reshape2::melt(id.vars = "dates") %>%
-                dplyr::mutate(DATE = .data$dates %>% as.Date()) %>%
-                dplyr::filter(.data$DATE >= stdt)),
-      ggplot2::aes(x = .data$DATE, y = .data$value, color = .data$variable),
-      size = 1)
-
-
+  # Define color palette
   p <- p +
-
     ggplot2::scale_color_manual(
       values = c(FALSE_EVAL_LO           = 'blue',
                  FALSE_EVAL_HI           = 'blue',
@@ -86,22 +85,15 @@ recommendation_plot <- function(ticker) {
                  TRUE_EVAL_HI            = 'red',
                  FALSE_BUY_LO            = 'green',
                  FALSE_BUY_HI            = 'green',
-                 PX_LAST                 = 'black',
-                 BEST_TARGET_PRICE       = 'gold')) +
+                 px_last                 = 'black',
+                 best_target_price       = 'gold'))
 
-    ggplot2::ggtitle(paste(stringr::str_to_upper(ticker), 'Recommendation & Range History')) +
-    ggplot2::theme(
-      plot.title = ggplot2::element_text(face = "bold"),
-      legend.position = "none",
-      axis.title = ggplot2::element_blank()
-    )
-
+  # Format plot area
   p <- p %>%
     pamngr::pam_plot(
-      plot_title = paste(stringr::str_to_upper(ticker), "Recommendation History"),
+      plot_title  = paste(stringr::str_to_upper(ticker), "Recommendation History"),
       show_legend = FALSE,
-      caption = FALSE
-    )
+      caption     = FALSE)
 
   return(p)
 }
