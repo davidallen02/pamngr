44583cc8581ccbf477a4f5309be626f26a2b823f
David Allen <58182518+davidallen02@users.noreply.github.com>
2021-05-24 07:46:53 -0700

    change dropbox path

--- R/update-data.R
+++ R/update-data.R
@@ -5,16 +5,16 @@
 update_data <- function(security){
 
   # search for security
-  equities <- dir("C:/Users/David/Dropbox/pam/asset-management/equities")
-  economics <- dir("C:/Users/David/Dropbox/pam/economics")
+  equities <- dir("C:/Users/David/Dropbox/work/pam/asset-management/equities")
+  economics <- dir("C:/Users/David/Dropbox/work/pam/economics")
 
   if(security %in% equities){
     # path <- "R:/David/asset-management/equities/"
-    path <- "C:/Users/David/Dropbox/pam/asset-management/equities"
+    path <- "C:/Users/David/Dropbox/pam/work/asset-management/equities"
   } else {
     if(security %in% economics){
       # path <- "R:/David/economics/data"
-      path <- "C:/Users/David/Dropbox/pam/economics"
+      path <- "C:/Users/David/Dropbox/work/pam/economics"
     } else{
       print(paste(security, "not in either equities or economics folders"))
     }
