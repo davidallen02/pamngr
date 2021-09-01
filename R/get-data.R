429454875ab111a5e2a23213a96ed5d342a53f4f
David Allen <davidallen02@me.com>
2021-09-01 11:03:25 -0700

    Update get-data.R

--- R/get-data.R
+++ R/get-data.R
@@ -39,7 +39,7 @@ get_data <- function(ticker,
       ticker_full <- ticker %>%
         stringr::str_replace_all("-", " ") %>%
         stringr::str_to_upper() %>%
-        paste(type)
+        paste(stringr::str_to_title(type))
 
       field_full <- flds %>%
         stringr::str_replace_all("-", "_") %>%
@@ -68,19 +68,24 @@ get_data <- function(ticker,
           stringr::str_to_lower() %>%
           stringr::str_replace_all(" ", "-")
 
-        saveRDS(dat, paste0("C:/Users/David/Dropbox/work/pam/economics/eco-data/data/", file_name, ".RDS"))
-        saveRDS(dat, paste0("C:/Users/David/PAM Research Dropbox/David/data/eco-data/", file_name, ".RDS"))
+        saveRDS(dat, paste0(pamngr::get_path(), "data/eco-data/", file_name, ".RDS"))
       }
 
       if(type == "Equity"){
 
-        ticker <- ticker %>% stringr::word() %>% stringr::str_to_lower()
+        # ticker <- ticker %>% stringr::word() %>% stringr::str_to_lower()
+        #
+        # flds <- flds %>% stringr::str_to_lower() %>% stringr::str_replace_all("_", "-")
 
-        flds <- flds %>% stringr::str_to_lower() %>% stringr::str_replace_all("_", "-")
+        # file_name <- paste0("C:/Users/David/Dropbox/work/pam/asset-management/equities/eq-data/output/",
+        #                     ticker, "-(", flds,").RDS")
 
-        file_name <- paste0("C:/Users/David/Dropbox/work/pam/asset-management/equities/eq-data/output/",
-                            ticker, "-(", flds,").RDS")
-        dat <- readRDS(file_name)
+        file_name <- ticker %>%
+          paste0("-(", flds, ")") %>%
+          stringr::str_replace_all("_", "-") %>%
+          stringr::str_to_lower()
+
+        saveRDS(dat, paste0(pamngr::get_path(), "data/eq-data", file_name, ".RDS"))
       }
 
     } else {
