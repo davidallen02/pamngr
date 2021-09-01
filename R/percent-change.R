88479f15c4ac164677bc4327ed73735cc891110c
David Allen <davidallen02@me.com>
2021-09-01 09:51:55 -0700

    Update percent-change.R

--- R/percent-change.R
+++ R/percent-change.R
@@ -21,35 +21,96 @@ pchange <- function(dat, k = 1, trading_days = FALSE){
   }
 
   if(is.character(k)){
-    compare_date <- k %>% paste("00:00:00") %>% lubridate::as_datetime()
 
-    if(trading_days){
-      if(weekdays(compare_date) == "Saturday"){
-        compare_date <- compare_date - lubridate::days(1)}
+    period_change <- function(dat, k = lubridate::years(1)){
 
-      if(weekdays(compare_date) == "Sunday"){
-        compare_date <- compare_date - lubridate::days(2)}
+      min_date <- dat %>%
+        dplyr::slice_min(dates, n = 1) %>%
+        dplyr::select(dates) %>%
+        dplyr::pull() %>%
+        print()
+
+      dat <- dat %>%
+        dplyr::mutate(
+          compare_date = dates %m-% k,
+          found = compare_date %in% dates,
+          in_range = compare_date >= min_date)
+
+
+      # while(sum(dat$check) + out_of_range != length(dat$dates)){
+#
+#       for(i in 1:5){
+#
+#         dat_yes <- dat %>% dplyr::filter(found | !in_range)
+#         dat_no <- dat %>% dplyr::filter(!found & in_range)
+#
+#         print(length(dat_no$dates))
+#
+#
+#         dat_no <- dat_no %>%
+#           dplyr::mutate(
+#             compare_date = compare_date %>%
+#               magrittr::subtract(lubridate::days(1)),
+#             check = compare_date %in% dat$dates
+#           )
+#
+#         dat <- rbind(dat_yes, dat_no)
+#       }
+#
+#       print(dat_no)
+#
+#       # dat <- dat %>% filter(dates >= min_date)
+#
+#       return(dat)
+#
+#     }
+#
+#     if(stringr::str_detect(k, "lubridate::")){
+#       dat <- dat %>%
+#         dplyr::mutate(
+#           compare_date = dates %>% magrittr::subtract(k),
+#           check = compare_date %>% is.na() %>% as.numeric(),
+#           compare_date = dates %>%
+#             magrittr::subtract(k) %>%
+#             magrittr::subtract(lubridate::days(check))
+#           )
+#
+#         )
+#
+#     } else {
+      compare_date <- k %>% paste("00:00:00") %>% lubridate::as_datetime()
+
+      if(trading_days){
+        if(weekdays(compare_date) == "Saturday"){
+          compare_date <- compare_date - lubridate::days(1)}
+
+        if(weekdays(compare_date) == "Sunday"){
+          compare_date <- compare_date - lubridate::days(2)}
+      }
+
+      fcompare_date <- "d" %>%
+        paste0(compare_date) %>%
+        stringr::str_remove_all("-") %>%
+        rlang::sym()
+
+      dat <- dat %>%
+        tidyr::pivot_wider(names_from = .data$dates, names_prefix = "d") %>%
+        dplyr::rename_all(stringr::str_remove_all,"-") %>%
+        dplyr::mutate(compare_date = (!!fcompare_date)) %>%
+        dplyr::mutate(dplyr::across(-.data$variable, ~ ./compare_date)) %>%
+        dplyr::select(-compare_date) %>%
+        tidyr::pivot_longer(cols      = -.data$variable,
+                            names_to  = "dates",
+                            values_to = "value") %>%
+        dplyr::mutate(
+          dates = .data$dates %>%
+            stringr::str_remove_all("d") %>%
+            lubridate::as_datetime(format = "%Y%m%d"),
+          value = .data$value %>% magrittr::subtract(1)
+        ) %>%
+        dplyr::select(.data$dates, .data$variable, .data$value)
     }
 
-    fcompare_date <- "d" %>%
-      paste0(compare_date) %>%
-      stringr::str_remove_all("-") %>%
-      rlang::sym()
-
-    dat <- dat %>%
-      tidyr::pivot_wider(names_from = .data$dates, names_prefix = "d") %>%
-      dplyr::rename_all(stringr::str_remove_all,"-") %>%
-      dplyr::mutate(compare_date = (!!fcompare_date)) %>%
-      dplyr::mutate(dplyr::across(-.data$variable, ~ ./compare_date)) %>%
-      dplyr::select(-compare_date) %>%
-      tidyr::pivot_longer(cols = -.data$variable, names_to = "dates", values_to = "value") %>%
-      dplyr::mutate(
-        dates = .data$dates %>%
-          stringr::str_remove_all("d") %>%
-          lubridate::as_datetime(format = "%Y%m%d"),
-        value = .data$value %>% magrittr::subtract(1)
-      ) %>%
-      dplyr::select(.data$dates, .data$variable, .data$value)
   }
 
 
